b824cd8de0e55c4f604fec2bcf7e8ee1f211f77d
Bombwhale <solkblte@icloud.com>
2022-08-02 09:54:35 +0900

    remove legacy files

--- app/src/components/Buttons.js
+++
@@ -1,9 +0,0 @@
-const AskYesOrNo = ({setYN, question, answerYes, answerNo}) => {
-    return <div>
-        <p>{question}</p>
-        <p onClick={() => setYN(true)}>{answerYes} (Y)</p>
-        <p onClick={() => setYN(false)}>{answerNo} (N)</p>
-    </div>
-}
-
-export {AskYesOrNo}
